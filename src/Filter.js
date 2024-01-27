74d0bdddbdc1ca22bd81fea0b8b186e415da559c
Olajutemo Timilehin <olajutemot@gmail.com>
2024-01-27 05:15:01 -0800

    update

--- src/Filter.js
+++ src/Filter.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import Skills from "./Skills";
-// import Project from "./Project";
 
 const Filter = ({ handleSkills, handleProjects, skills, projects }) => {
   return (
@@ -32,72 +30,5 @@ const Filter = ({ handleSkills, handleProjects, skills, projects }) => {
     </div>
   );
 };
-
-// const Filter = ({ updatedSkills }) => {
-//   //   const [skills, setSkills] = useState("");
-//   //   const [projects, setProjects] = useState("");
-//   // const updatedSkill = updatedSkills;
-//   // const handleSkills = () => updatedSkill;
-
-//   //   const handleProjects = () => {
-//   //     setProjects(true);
-//   //   };
-
-//   const [skills, setSkills] = useState(true);
-//   const [projects, setProjects] = useState(true);
-//   const handleSkills = () => {
-//     setSkills(false);
-//   };
-//   const handleProjects = () => {
-//     setSkills(true);
-//     setProjects(true);
-//   };
-//   return (
-//     <div class="filter-tabs">
-//       <a
-//         id="projects-button"
-//         className={
-//           skills && projects
-//             ? "project-toggle project-toggle-active"
-//             : "project-toggle"
-//         }
-//         href="#projects"
-//         onClick={handleProjects}
-//       >
-//         Projects{" "}
-//       </a>
-//       <a
-//         id="skills-button"
-//         className={
-//           skills && projects
-//             ? "project-toggle"
-//             : "project-toggle project-toggle-active"
-//         }
-//         href="#skills"
-//         onClick={handleSkills}
-//       >
-//         Skills
-//       </a>
-//     </div>
-//     // <div class="filter-tabs">
-//     //   <a
-//     //     id="projects-button"
-//     //     class="project-toggle project-toggle-active"
-//     //     href="#projects"
-//     //     // onClick={handleProjects}
-//     //   >
-//     //     Projects
-//     //   </a>
-//     //   <a
-//     //     id="skills-button"
-//     //     class="project-toggle"
-//     //     href="#skills"
-//     //     onClick={handleSkills}
-//     //   >
-//     //     Skills
-//     //   </a>
-//     // </div>
-//   );
-// };
 
 export default Filter;
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,15 +1,16 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Project from "./Project";
 import Skills from "./Skills";
 import Socials from "./Socials";
 import Info from "./Info";
 import Filter from "./Filter";
+import Hompage from "./Hompage";
 
 const Portfolio = () => {
   const [skills, setSkills] = useState(true);
   const [projects, setProjects] = useState(true);
+  const [close, setClose] = useState(false);
   const handleSkills = () => {
     setSkills(false);
   };
@@ -17,199 +18,49 @@ const Portfolio = () => {
     setSkills(true);
     setProjects(true);
   };
+  const handleClose = () => {
+    setClose(<Hompage />);
+  };
   return (
-    <div class="portfolio lightmode" id="portfolioOverlay">
-      <div class="portfolio-container">
-        <img id="portfolio-img" src="menu/Timmy Ai.png" alt="Timilehin" />
-        <Socials />
-        <Info />
-        <Filter
-          handleSkills={handleSkills}
-          handleProjects={handleProjects}
-          skills={skills}
-          projects={projects}
-        />
-        {/* <div class="filter-tabs">
-          <a
-            id="projects-button"
-            className={
-              skills && projects
-                ? "project-toggle project-toggle-active"
-                : "project-toggle"
-            }
-            href="#projects"
-            onClick={handleProjects}
-          >
-            Projects{" "}
-          </a>
-          <a
-            id="skills-button"
-            className={
-              skills && projects
-                ? "project-toggle"
-                : "project-toggle project-toggle-active"
-            }
-            href="#skills"
-            onClick={handleSkills}
-          >
-            Skills
-          </a>
-        </div> */}
-        <section class="filter-sections">
-          <Project
-            handleSkills={handleSkills}
-            handleProjects={handleProjects}
-            skills={skills}
-            projects={projects}
-          />
-          {/* <section
-            className={skills && projects ? "grid-project-item" : "hidden"}
-            id="projects"
-          >
-            <div class="project-items">
-              <img src="menu/Project tile 1.png" alt="Project 1" />
-              <div class="project-overlay">
-                <h3>Web App</h3>
-                <p>Dice game</p>
-                <i class="fa-solid fa-up-right-from-square"></i>
-              </div>
-            </div>
-            <div class="project-items">
-              <img src="menu/project tile 2.png" alt="Project 2" />
-              <div class="project-overlay">
-                <h3>Web</h3>
-                <p>interactive and responsive website</p>
-                <i class="fa-solid fa-up-right-from-square"></i>
-              </div>
-            </div>
-            <div class="project-items">
-              <img src="menu/project tile 4.png" alt="Project 4" />
-              <div class="project-overlay">
-                <h3>Web</h3>
-                <p>Responsive website</p>
-                <i class="fa-solid fa-up-right-from-square"></i>
-              </div>
-            </div>
-            <div class="project-items">
-              <img src="menu/project tile 5.png" alt="Project 5" />
-              <div class="project-overlay">
-                <h3>Web</h3>
-                <p>Burger Description website</p>
-                <i class="fa-solid fa-up-right-from-square"></i>
-              </div>
-            </div>
-          </section> */}
-          <Skills
-            handleSkills={handleSkills}
-            handleProjects={handleProjects}
-            skills={skills}
-            projects={projects}
-          />
-          {/* <section
-            className={skills && projects ? "hidden" : "skills-container"}
-            id="skills"
-          >
-            <div class="frontend">
-              <h3>Frontend Developer</h3>
+    <div>
+      {close || (
+        <div class="portfolio lightmode" id="portfolioOverlay">
+          <div class="portfolio-container">
+            <img id="portfolio-img" src="menu/Timmy Ai.png" alt="Timilehin" />
+            <Socials />
+            <Info />
+            <Filter
+              handleSkills={handleSkills}
+              handleProjects={handleProjects}
+              skills={skills}
+              projects={projects}
+            />
 
-              <div class="Skills-grid">
-                <div>
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">HTML</h3>
-                      <p class="skill-level">Advanced</p>
-                    </div>
-                  </article>
+            <section class="filter-sections">
+              <Project
+                handleSkills={handleSkills}
+                handleProjects={handleProjects}
+                skills={skills}
+                projects={projects}
+              />
 
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">CSS</h3>
-                      <p class="skill-level">Intermediate</p>
-                    </div>
-                  </article>
-
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">JAVASCRIPT</h3>
-                      <p class="skill-level">Intermediate</p>
-                    </div>
-                  </article>
-                </div>
-
-                <div>
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">REACT</h3>
-                      <p class="skill-level">Basic</p>
-                    </div>
-                  </article>
-
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">BOOTSTRAP</h3>
-                      <p class="skill-level">Basic</p>
-                    </div>
-                  </article>
-
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">GIT</h3>
-                      <p class="skill-level">Intermediate</p>
-                    </div>
-                  </article>
-                </div>
-              </div>
-            </div>
-            <div class="backend">
-              <h3>Backend Developer</h3>
-              <div class="Skills-grid">
-                <div>
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">NODE JS</h3>
-                      <p class="skill-level">Basic</p>
-                    </div>
-                  </article>
-
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">PYTHON</h3>
-                      <p class="skill-level">Basic</p>
-                    </div>
-                  </article>
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">MY SQL</h3>
-                      <p class="skill-level">Basic</p>
-                    </div>
-                  </article>
-                </div>
-                <div>
-                  <article class="skill-details">
-                    <i class="fa-regular fa-circle-check fa-2xs"></i>
-                    <div>
-                      <h3 class="skill-name">MY SQL</h3>
-                      <p class="skill-level">Basic</p>
-                    </div>
-                  </article>
-                </div>
-              </div>
-            </div>
-          </section> */}
-        </section>
-        <button className="button" id="closePortfolio">
-          Close
-        </button>
-      </div>
+              <Skills
+                handleSkills={handleSkills}
+                handleProjects={handleProjects}
+                skills={skills}
+                projects={projects}
+              />
+            </section>
+            <button
+              className="button"
+              id="closePortfolio"
+              onClick={handleClose}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
