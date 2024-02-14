c23bf24e5bd01bfb9b6d4479988c909479cf8ccc
Olajutemo Timilehin <olajutemot@gmail.com>
2024-02-14 10:57:58 -0800

    update

--- src/BlogSite.js
+++ src/BlogSite.js
@@ -6,7 +6,7 @@ const BlogSite = () => {
     <div className={styles.Red}>
       <h1>Hi welcome to my blog page</h1>
       <p>
-        This is still a work in progress, kindly enter your email below to get a
+        This is still in progress, kindly enter your email below to get a
         notification when the page is lived
       </p>
       <input type="email" placeholder="enter your email" />
