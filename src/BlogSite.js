67ef6987bcc56a7016a8b7a9d3a7f1a99dc73ecb
Olajutemo Timilehin <olajutemot@gmail.com>
2024-02-21 20:19:11 -0800

    blog update

--- src/BlogSite.js
+++ src/BlogSite.js
@@ -3,17 +3,18 @@ import styles from "./Blog.module.css";
 
 const BlogSite = () => {
   return (
-    <div className={styles.Red}>
-      <h1>Hi welcome to my blog page</h1>
-      <p>
-        This is still in progress, kindly enter your email below to get a
-        notification when the page is lived
-      </p>
-      <input type="email" placeholder="enter your email" />
-
-      <h2>
-        click this to <button>Go back</button> or reload page to go back
-      </h2>
+    <div>
+      <nav className={styles.Navbar}>
+        <div className={styles.Icons}>
+          <i class="fa-solid fa-magnifying-glass"></i>
+          <i class="fa-solid fa-plus"></i>
+          <i class="fa-regular fa-user"></i>
+        </div>
+        <div className="anchors">
+          <a href="/">Home</a>
+          <a href="/">about</a>
+        </div>
+      </nav>
     </div>
   );
 };
