3284740f9aad11c37268284c4981983257f49af5
Olajutemo Timilehin <olajutemot@gmail.com>
2024-03-05 06:13:36 -0800

    signup Api integration from backend

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -1,17 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Auth.module.css";
 
 // const width = window.innerWidth;
 // const height = window.innerHeight;
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setErrors({});
+
+    const validationErrors = {};
+    if (!formData.email) {
+      validationErrors.email = "Email is required";
+    }
+    if (!formData.password) {
+      validationErrors.password = "Password is required";
+    }
+    if (formData.password !== formData.confirmPassword) {
+      validationErrors.confirmPassword = "Passwords do not match";
+    }
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        if (response.ok) {
+          alert("Registration successful!");
+          setFormData({
+            name: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+          });
+        } else {
+          alert(`Registration failed: ${response.message}`);
+        }
+      })
+      .catch((error) => {
+        console.error("Registration error:", error.message);
+        alert("Registration failed. Please try again later.");
+      });
+  };
   return (
     <div className={styles.formContainer}>
       <h1>Register Admin</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label for="name">Name: </label>
-          <input type="text" id="name" placeholder="Enter your name" />
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter your name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          {errors.name && <span className="formerror">{errors.name}</span>}
         </div>
         <div>
           <label for="email">Email: </label>
@@ -19,7 +89,11 @@ const SignUp = () => {
             type="text"
             id="email"
             placeholder="Enter your email address"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
           />
+          {errors.email && <span className="formerror">{errors.email}</span>}
         </div>
         <div>
           <label for="password">Password: </label>
@@ -27,13 +101,28 @@ const SignUp = () => {
             type="password"
             id="password"
             placeholder="Enter your Password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
           />
+          {errors.password && (
+            <span className="formerror">{errors.password}</span>
+          )}
         </div>
         <div>
           <label>Confirm Password: </label>
-          <input type="password" placeholder="Enter password again" />
+          <input
+            type="password"
+            placeholder="Enter password again"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+          {errors.confirmPassword && (
+            <span className="formerror">{errors.confirmPassword}</span>
+          )}
         </div>
-        <button>Register</button>
+        <button type="submit">Register</button>
       </form>
       <p>
         Already have an account? click <a href="/log-in">here to login</a>
